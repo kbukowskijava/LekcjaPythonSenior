66a2eefc8a3d2071af11faba549e3bd43cd48518
Kacper Bukowski <kacper.bukowski@student.wat.edu.pl>
2022-06-06 20:11:56 +0200

    Zakończenie zajęć 6.06.2022

--- Lekcja 8/client.py
+++ Lekcja 8/client.py
@@ -7,31 +7,42 @@ from tkinter import messagebox
 with open('config.json') as json_file:
     config = json.load(json_file)
 
+
 def receive():
     while True:
         try:
             msg = CLIENT.recv(BUFSIZE).decode("utf8")
             n = 60
-            for i in range(0,len(msg), 60):
+            for i in range(0, len(msg), 60):
                 msg_list.insert(tk.END, msg[i:(i + n)])
         except OSError:
             break
 
+
 def send(event=None):
     msg = my_msg.get()
-    #Czyszczenie pola wprowadzania wiadomości
+    # Czyszczenie pola wprowadzania wiadomości
     my_msg.set('')
     CLIENT.send(bytes(msg, 'utf8'))
+
+
+def on_closing(event=None):
+    if messagebox.askokcancel('Quit', 'Czy chcesz zakończyć?'):
+        my_msg.set('{quit}')
+        send()
+        CLIENT.close()
+        main_window.quit()
+
 
 main_window = tk.Tk()
 main_window.title('Chat')
 main_window.geometry('450x450')
-main_window.resizable(False,False)
+main_window.resizable(False, False)
 
 main_window.option_add('*Font', '{Calibri} 10')
 main_window.option_add('*Background', 'white')
 
-#pole tekstowe do wprowadzania wiadomości
+# pole tekstowe do wprowadzania wiadomości
 message_frame = tk.Frame(main_window)
 my_msg = tk.StringVar()
 scrollbar = tk.Scrollbar(message_frame)
@@ -46,6 +57,7 @@ entry_field = tk.Entry(main_window, width=62, textvariable=my_msg)
 entry_field.bind("<Return>", send)
 entry_field.pack()
 send_button = tk.Button(main_window, text="Wyślij", width=15, command=send)
+send_button.pack()
 
 main_window.protocol("WM_DELETE_WINDOW", on_closing)
 
@@ -54,5 +66,10 @@ PORT = config['port']
 BUFSIZE = 1024
 ADDR = (HOST, PORT)
 
-CLIENT = (AF_INET, SOCK_STREAM)
+CLIENT = socket(AF_INET, SOCK_STREAM)
 CLIENT.connect(ADDR)
+
+RECEIVE_THREAD = Thread(target=receive)
+RECEIVE_THREAD.start()
+
+main_window.mainloop()
